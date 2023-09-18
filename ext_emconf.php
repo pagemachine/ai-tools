2c945808b159d81431a37cea97b48d34515c0000
Sascha Löffler <sloeffler@pagemachine.de>
2023-09-18 14:43:08 +0200

    [TASK] Add emconf for TER, change version

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'AI Tools',
+    'description' => 'Use A.I. Tools in the TYPO3 backend. Generate Images, Image Variants and Describe Images.',
+    'category' => 'module',
+    'author' => 'Sascha Löffler',
+    'author_email' => 'sloeffler@pagemachine.de',
+    'author_company' => 'Pagemachine AG',
+    'state' => 'alpha',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.0.0-11.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
