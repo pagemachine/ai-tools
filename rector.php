e8da9a664570750c91546acf3e3419abae130a1c
Sascha Löffler <sloeffler@pagemachine.de>
2024-07-04 13:34:39 +0200

    [BUGFIX] Rector fixes

--- rector.php
+++ rector.php
@@ -7,6 +7,7 @@ use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Renaming\Rector\ClassConstFetch\RenameClassConstFetchRector;
 use Ssch\TYPO3Rector\Set\Typo3SetList;
+use Ssch\TYPO3Rector\TYPO312\v3\MigrateMagicRepositoryMethodsRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -26,7 +27,9 @@ return RectorConfig::configure()
     ])
     ->withSkip([
         AddLiteralSeparatorToNumberRector::class,
+        __DIR__ . '/Classes/Controller/Backend/ImageCreationController.php', // currently disabled
         RenameClassConstFetchRector::class => [
             __DIR__ . '/packages/**/ExternalImport/*', // Skip invalid AbstractMessage::* migration
         ],
+        MigrateMagicRepositoryMethodsRector::class,// ignore for now to support TYPO3 v11
     ]);
