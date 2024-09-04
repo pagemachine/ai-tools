5bb10f58489966ec70d5002471253f2350df539a
Francisco Seipel <fseipel@pagemachine.de>
2024-09-04 11:31:21 +0200

    [TASK] Remove typo3 12 from rector as it should support 11

--- rector.php
+++ rector.php
@@ -3,11 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Renaming\Rector\ClassConstFetch\RenameClassConstFetchRector;
 use Ssch\TYPO3Rector\Set\Typo3SetList;
-use Ssch\TYPO3Rector\TYPO312\v3\MigrateMagicRepositoryMethodsRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -23,13 +20,7 @@ return RectorConfig::configure()
     ->withSets([
         PHPUnitSetList::PHPUNIT_100,
         Typo3SetList::TYPO3_11,
-        Typo3SetList::TYPO3_12,
     ])
     ->withSkip([
-        AddLiteralSeparatorToNumberRector::class,
-        __DIR__ . '/Classes/Controller/Backend/ImageCreationController.php', // currently disabled
-        RenameClassConstFetchRector::class => [
-            __DIR__ . '/packages/**/ExternalImport/*', // Skip invalid AbstractMessage::* migration
-        ],
-        MigrateMagicRepositoryMethodsRector::class, // ignore for now to support TYPO3 v11
+
     ]);
