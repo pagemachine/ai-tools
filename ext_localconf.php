7107faf6bda713f3aee6a1929f7f787994ae7330
Francisco Seipel <fseipel@pagemachine.de>
2024-09-09 09:49:22 +0200

    [TASK] Make sure only one default prompt can exist

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types=1);
-use Pagemachine\AItools\ContextMenu\ItemProviders\AiToolItemProvider;
 
+use Pagemachine\AItools\ContextMenu\ItemProviders\AiToolItemProvider;
+use Pagemachine\AItools\Hooks\DataHandlerHooks;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
@@ -14,3 +15,6 @@ if (version_compare($version, '11.0', '>=') && version_compare($version, '12.0',
     // for TYPO3 v11
     $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1693997897] = AiToolItemProvider::class;
 }
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['aitools'] = DataHandlerHooks::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['aitools'] = DataHandlerHooks::class;
