095260ec8e3d6e4865219afaefe36da027535ace
Francisco Seipel <fseipel@pagemachine.de>
2024-09-24 12:24:14 +0200

    [TASK] Pass result from modal to tca

--- Resources/Public/JavaScript/AlternativeGenerator.js
+++ Resources/Public/JavaScript/AlternativeGenerator.js
@@ -24,14 +24,33 @@ define(['jquery', 'TYPO3/CMS/Backend/Modal', 'TYPO3/CMS/AiTools/RemoteCalls'], f
   };
 
   AlternativeGenerator.show = function (trigger) {
-    Modal.advanced({
+    const currentModal = Modal.advanced({
       additionalCssClasses: ['modal-image-manipulation'],
       buttons: [
       ],
+      staticBackdrop: true,
       content: trigger.data('base') + '&target=' + encodeURIComponent(trigger.data('target')) + '&target-language=' + encodeURIComponent(trigger.data('target-language')),
       type: Modal.types.ajax,
       size: Modal.sizes.large,
       title: trigger.data('title'),
+    });
+
+    currentModal.css('pointer-events', 'none');
+
+    currentModal.on('modal-loaded', function () {
+      const use = currentModal.find('.t3js-alternative-use-trigger');
+      const save = currentModal.find('.t3js-alternative-save-trigger');
+      const target = $(trigger.data('output-target'));
+
+      use.show();
+      save.hide();
+
+      use.off('click').on('click', function (e) {
+        const value = currentModal.find('.textarea-altTextSuggestion').first().val();
+        target.val(value);
+        target.trigger('change');
+        currentModal.modal('hide');
+      });
     });
   };
 
