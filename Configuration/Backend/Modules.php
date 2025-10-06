9504c7f8272fdd789d16d86f5ad1506f4465d00f
Francisco Seipel <fseipel@pagemachine.de>
2025-10-06 15:05:51 +0200

    [TASK] Remove settingsController

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -2,7 +2,6 @@
 
 use Pagemachine\AItools\Controller\Backend\PromptsController;
 use Pagemachine\AItools\Controller\Backend\ServersController;
-use Pagemachine\AItools\Controller\Backend\SettingsController;
 
 return [
     'AItoolsAitools' => [
@@ -25,7 +24,7 @@ return [
             ],
         ],
     ],
-    'AItoolsAitools_AItoolsSettings' => [
+    'AItoolsAitools_AItoolsServers' => [
         'parent' => 'AItoolsAitools',
         'position' => ['before' => '*'],
         'access' => 'admin',
@@ -33,22 +32,6 @@ return [
         'iconIdentifier' => 'module-install-settings',
         'path' => '/module/aitools/AItoolsSettings',
         'labels' => 'LLL:EXT:ai_tools/Resources/Private/Language/BackendModules/locallang_be_settings.xlf',
-        'extensionName' => 'AItools',
-        'controllerActions' => [
-            SettingsController::class => [
-                'settings',
-                'save',
-            ],
-        ],
-    ],
-    'AItoolsAitools_AItoolsServers' => [
-        'parent' => 'AItoolsAitools',
-        'position' => ['before' => '*'],
-        'access' => 'admin',
-        'workspaces' => 'live',
-        'iconIdentifier' => 'apps-filetree-mount',
-        'path' => '/module/aitools/AItoolsServers',
-        'labels' => 'LLL:EXT:ai_tools/Resources/Private/Language/locallang_db.xlf:tx_aitools_domain_model_server.servers',
         'extensionName' => 'AItools',
         'controllerActions' => [
             ServersController::class => [
