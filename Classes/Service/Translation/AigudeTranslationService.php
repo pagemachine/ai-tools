1c0e239f374fa1f0fc0df21046dbb1ceba254e33
Francisco Seipel <fseipel@pagemachine.de>
2025-11-18 12:12:37 +0100

    [TASK] Remove LanguageScript completely

--- Classes/Service/Translation/AigudeTranslationService.php
+++ Classes/Service/Translation/AigudeTranslationService.php
@@ -5,15 +5,11 @@ declare(strict_types=1);
 namespace Pagemachine\AItools\Service\Translation;
 
 use Pagemachine\AItools\Service\Abstract\AigudeAbstract;
-use Pagemachine\AItools\Utility\LanguageScriptUtility;
 
 class AigudeTranslationService extends AigudeAbstract implements TranslationServiceInterface
 {
     public function sendTranslationRequestToApi(string $text, string $sourceLang = 'en', string $targetLang = 'en'): string
     {
-        $sourceLang = LanguageScriptUtility::getLanguageScript($sourceLang);
-        $targetLang = LanguageScriptUtility::getLanguageScript($targetLang);
-
         $url = $this->domain.'/translate';
 
         // Prepare the form data
