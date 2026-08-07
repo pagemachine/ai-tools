ff02e8292651f9b3fbe80bae8e05a26140bd2f71
Mauricio Altamirano Silva <mauricio.as@gmail.com>
2026-08-07 13:35:24 +0200

    [TASK] Replace emoji and version-dependent icons with core icons

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -14,6 +14,11 @@ return [
         'provider' => BitmapIconProvider::class,
         'source' => 'EXT:ai_tools/Resources/Public/Icons/ext_icon.png',
     ],
+    // Own icon: core's actions-star is filled on v12/v13 but an outline on v14.
+    'tx-aitools-star' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:ai_tools/Resources/Public/Icons/star.svg',
+    ],
     'tx-aitools-module' => [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:ai_tools/Resources/Public/Icons/ext_module.svg',
